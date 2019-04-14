a901c399632bc9dd49c83a897dc3162cc1929c29
Samira Munir <42713239+samiramunir@users.noreply.github.com>
2019-04-13 22:20:17 -0400

    wrote create table and drop table queries in sql_queries, upadted passworda and username inputs in other files

--- create_tables.py
+++ create_tables.py
@@ -1,24 +1,30 @@
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
+config = {}
+exec(open('config.py').read(), config)
+user = config['user']
+user2 = config['user2']
+password = config['password']
 
 def create_database():
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect(f"host=127.0.0.1 dbname=db1 user={user} password={password}")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
-    
+
     # create sparkify database with UTF8 encoding
     cur.execute("DROP DATABASE IF EXISTS sparkifydb")
     cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    # cur.execute("")
 
     # close connection to default database
-    conn.close()    
-    
+    conn.close()
+
     # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(f"host=127.0.0.1 dbname=sparkifydb user={user} password={password}")
     cur = conn.cursor()
-    
+
     return cur, conn
 
 
@@ -36,7 +42,7 @@ def create_tables(cur, conn):
 
 def main():
     cur, conn = create_database()
-    
+
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,47 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
+
 songplay_table_create = ("""
+                            CREATE TABLE IF NOT EXISTS
+                                songplays (songplay_id int, start_time timestamp,
+                                            user_id int NOT NULL, level text,
+                                            song_id varchar, artist_id varchar,
+                                            session_id int, location text,
+                                            user_agent text)
 """)
 
 user_table_create = ("""
+                            CREATE TABLE IF NOT EXISTS
+                                users (user_id int NOT NULL, first_name text,
+                                        last_name text, gender text,
+                                        level text)
+
 """)
 
 song_table_create = ("""
+                            CREATE TABLE IF NOT EXISTS
+                                songs (song_id varchar NOT NULL, title text,
+                                        artist_id varchar NOT NULL, year int,
+                                        duration numeric)
 """)
 
 artist_table_create = ("""
+                            CREATE TABLE IF NOT EXISTS
+                                artists (artist_id varchar NOT NULL, name text,
+                                            location text, lattitude numeric, longitude numeric)
 """)
 
 time_table_create = ("""
+                            CREATE TABLE IF NOT EXISTS
+                            time (start_time timestamp, hour int, day int, week int, month int, year int, weekday boolean)
 """)
 
 # INSERT RECORDS
